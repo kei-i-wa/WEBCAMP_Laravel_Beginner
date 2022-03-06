adc37ed215ced3cdc3cd9951376b72c46c1481fa
keiiwasaki <kei.kumarg@gmail.com>
2022-03-06 01:11:05 +0000

    タスクの登録処理まで書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,8 +1,10 @@
 <?php
 declare(strict_types=1);
 namespace App\Http\Controllers;
-
+use App\Http\Requests\TaskRegisterPostRequest;
+use App\Models\Task as TaskModel;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -15,5 +17,31 @@ class TaskController extends Controller
     {
         return view('task.list');
     }
+    
+     public function register(TaskRegisterPostRequest $request)
+    {
+        // validate済みのデータの取得
+        $datum = $request->validated();
+        //
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
 
+        // user_id の追加
+        $datum['user_id'] = Auth::id();
+
+        // テーブルへのINSERT
+ // テーブルへのINSERT
+        try {
+            $r = TaskModel::create($datum);
+            // var_dump($r); exit;
+        } catch(\Throwable $e) {
+            // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
+            echo $e->getMessage();
+            exit;
+        }
+        
+        $request->session()->flash('front.task_register_success', true);
+        return redirect('/task/list');
+    }
 }
