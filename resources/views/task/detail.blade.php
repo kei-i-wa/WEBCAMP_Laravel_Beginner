2e8e1f613cfd96037ebc6895571d09f7e9bcbc6e
keiiwasaki <kei.kumarg@gmail.com>
2022-03-07 11:54:32 +0000

    タスクの編集を書いた

--- resources/views/task/detail.blade.php
+++ resources/views/task/detail.blade.php
@@ -4,12 +4,16 @@
 @section('title')(詳細画面)@endsection
 
 {{-- メインコンテンツ --}}
-@section('contets')
+@section('contents')
         <h1>タスク詳細閲覧</h1>
-        タスク名:{{$task ->name }}<br>
-        期限:{{$task->period}}<br>
-        重要度:{{$task->getPriorityString()}}<br>
-        タスク詳細:<pre>{{$task->detail}}</pre>
+        @if (session('front.task_edit_success') == true)
+            タスクを編集しました！！<br>
+        @endif
+
+        タスク名： {{ $task->name }}<br>
+        期限： {{ $task->period }}<br>
+        重要度： {{ $task->getPriorityString() }}<br>
+        タスク詳細： <pre>{{ $task->detail }}</pre><br>
         <hr>
         <menu label="リンク">
         <a href="/task/list">タスク一覧</a><br>
